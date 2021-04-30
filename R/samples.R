a910e0502d1fc404c63d77410e8b94725b0add97
Mwavu <mwavukennedy@gmail.com>
2021-04-30 10:39:48 +0300

    Include sample boards as data

---
+++ R/samples.R
@@ -0,0 +1,18 @@
+#' Sample sudoku boards
+#'
+#' Each board is a 9 by 9 grid, with zeros representing empty spaces in a sudoku.
+#' Complexity increases as you go from board0 to the last board.
+#'
+#' @docType data
+#'
+#' @usage data(samples)
+#'
+#' @format A list object
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(samples)
+#' board0 <- samples[[0]]
+#' board0
+#' print_board(board0)
